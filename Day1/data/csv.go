dd35ac0ffcf342471e7085b4e95708f83da3b0b8
Benjamin Reigner <benjamin.reigner@epitech.eu>
2021-02-15 13:12:16 +0100

    [Day1] Ex2 Add completed exercise

--- Day1/data/csv.go
+++ Day1/data/csv.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"fmt"
 	"io/ioutil"
 	"strings"
 )
@@ -10,4 +11,13 @@ func ReadFile(filename string) ([]string, error) {
 	arr := strings.Split(string(text), "\n")
 
 	return arr, err
+}
+
+func LineToCSV(line string) ([]string, error) {
+	arr := strings.Split(line, ",")
+
+	if len(arr) == 0 {
+		return nil, fmt.Errorf("empty string")
+	}
+	return arr, nil
 }
--- Day1/main.go
+++ Day1/main.go
@@ -6,7 +6,15 @@ import (
 )
 
 func main() {
-	content, err := data.ReadFile("go.mod")
-	fmt.Println(err)
-	fmt.Println(content)
+	files := []string{"resources/big_csv.csv", "resources/example.csv", "resources/medium_csv.csv"}
+
+	for i := 0; i < len(files); i++ {
+		content, err := data.ReadFile(files[i])
+		for j := 0; j < len(content); j++ {
+			arr, err2 := data.LineToCSV(content[j])
+			fmt.Println(arr, err2)
+		}
+		fmt.Println(err)
+		fmt.Println(content)
+	}
 }
